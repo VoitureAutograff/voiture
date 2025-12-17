e07d0648f2367c34c09a7ed8cc5bf0f16af40ee7
VoitureAutograff <voiture.in@hotmail.com>
2025-12-17 23:48:39 +0530

    Update App.tsx
    added vercel speed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './router'
-
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <AppRoutes />
-    </BrowserRouter>
+    <>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <AppRoutes />
+      </BrowserRouter>
+      <SpeedInsights />
+    </>
   )
 }
 
